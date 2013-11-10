03ffcd614db668511c535f6e29972c0d206e2698
ninakatila <nkatila@gmail.com>
2013-11-10 21:32:45 +0200

    viikon 2 palautuksia

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>   
+    <head>
+        <meta http-equiv="Content-Type" content="text/html: charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+    </body>
+</html>    
